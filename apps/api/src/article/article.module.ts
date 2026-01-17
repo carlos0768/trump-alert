40416e8f8d9bbfd7a093bcd6e4bbce0c884c4403
carlos0768 <carlotti1208@gmail.com>
2026-01-17 17:36:29 +0900

    chore: auto-format and lint (TypeScript)

--- apps/api/src/article/article.module.ts
+++ apps/api/src/article/article.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
 import { StatsService } from './stats.service';
+import { FactCheckService } from './fact-check.service';
 import { CollectorModule } from '../collector/collector.module';
 import { AnalyzerModule } from '../analyzer/analyzer.module';
 
 @Module({
   imports: [CollectorModule, AnalyzerModule],
   controllers: [ArticleController],
-  providers: [ArticleService, StatsService],
-  exports: [ArticleService, StatsService],
+  providers: [ArticleService, StatsService, FactCheckService],
+  exports: [ArticleService, StatsService, FactCheckService],
 })
 export class ArticleModule {}
